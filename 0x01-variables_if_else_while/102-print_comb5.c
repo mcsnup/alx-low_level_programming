e5c958bf949fa8119e0333c3e5912bd9348eed4e
mcsnup <armstrong201@gmail.com>
2022-12-12 17:13:36 -0800

    matrix

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,18 +9,18 @@ int main(void)
 {
 	int i, j;
 
-	for (i = '0'; i < 100; i++)
+	for (i = 0; i < 100; i++)
 	{
 		for (j = 0; j < 100; j++)
 		{
-			if (i < j)
+			if (i != j)
 			{
 				putchar((i / 10) + 48);
 				putchar((i % 10) + 48);
 				putchar(' ');
 				putchar((j / 10) + 48);
 				putchar((j % 10) + 48);
-				if (i != 98 || j != 99)
+				if (i * 100 + j != 9899)
 				{
 					putchar(',');
 					putchar(' ');
@@ -29,5 +29,6 @@ int main(void)
 		}
 	}
 	putchar('\n');
+
 	return (0);
 }
