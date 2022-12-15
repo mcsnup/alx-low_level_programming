6b06bc5c1586e0e32d43c7770e46ec42dd555dfb
mcsnup <armstrong201@gmail.com>
2022-12-14 20:40:18 -0800

    natural nums

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,42 +7,16 @@
  */
 int main(void)
 {
-	int f = '0';
-	int s = '0';
-	int t = '0';
+	int natural = 1024, sum = 0, c;
 
-	while (f <= '9')
+	for (c = 0; c < natural; c++)
 	{
-		s = '0';
-		while (s <= '9')
+		if (c % 3 == 0 || c % 5 == 0)
 		{
-			t = '0';
-			while (t <= '9')
-			{
-				if (f == s || f == t || s == t)
-				{
-				}
-				else if (f > s || f > t || s > t)
-				{
-				}
-				else
-				{
-					putchar(f);
-					putchar(s);
-					putchar(t);
-					if (f == '7' && s == '8' && t == '9')
-					{
-						break;
-					}
-					putchar(',');
-					putchar(' ');
-				}
-				t++;
-			}
-			s++;
+			sum += c;
 		}
-		f++;
 	}
-	putchar('\n');
+
+	printf("%d\n", sum);
 	return (0);
 }
