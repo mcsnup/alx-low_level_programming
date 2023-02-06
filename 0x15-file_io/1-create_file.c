87042b613e6db9a6c13610556609c3c26deac702
mcsnup <37091089+mcsnup@users.noreply.github.com>
2023-02-06 08:31:06 +0100

    A function that creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * create_file - Entry point
+ *
+ * Description: A function that creates a file.
+ * @filename: Name of file to create
+ * @text_content: String to content file should contain.
+ * Return: 1 on success, -1 on failure.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	size_t len = 0;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (!text_content)
+	{
+		close(fd);
+		return (1);
+	}
+
+	while (text_content[len])
+		len++;
+
+	if (write(fd, text_content, len) == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return (1);
+}
